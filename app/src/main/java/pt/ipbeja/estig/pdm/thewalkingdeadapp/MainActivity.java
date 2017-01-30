7e22aa4b14cb03c1fd9194dabadd41fdfec8398d
RafaMede <rafael_medeiros_2012@hotmail.com>
2017-01-30 17:29:51 +0000

    Ponto 4

--- app/src/main/java/pt/ipbeja/estig/pdm/thewalkingdeadapp/MainActivity.java
+++ app/src/main/java/pt/ipbeja/estig/pdm/thewalkingdeadapp/MainActivity.java
@@ -2,6 +2,7 @@ package pt.ipbeja.estig.pdm.thewalkingdeadapp;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends FragmentActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "OnCreate", Toast.LENGTH_SHORT).show();
+        if (findViewById(R.id.fragment_container) != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+            ListFrag Frag = new ListFrag();
+            Frag.setArguments(getIntent().getExtras());
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, Frag).commit();
+        }
     }
 }
