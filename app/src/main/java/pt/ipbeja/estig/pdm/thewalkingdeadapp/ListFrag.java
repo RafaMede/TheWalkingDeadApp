56a4a9a0f0f31ac7c6243e9c23c6220351ef2ce0
RafaMede <rafael_medeiros_2012@hotmail.com>
2017-01-30 18:19:12 +0000

    Ponto 6

--- app/src/main/java/pt/ipbeja/estig/pdm/thewalkingdeadapp/ListFrag.java
+++ app/src/main/java/pt/ipbeja/estig/pdm/thewalkingdeadapp/ListFrag.java
@@ -3,15 +3,17 @@ package pt.ipbeja.estig.pdm.thewalkingdeadapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ListFrag extends Fragment {
+public class ListFrag extends ListFragment {
 
 
     public ListFrag() {
@@ -20,10 +22,18 @@ public class ListFrag extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        /**
+         * Creating an array adapter to host the list of items to be displayed in the ListFragment
+         */
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(inflater.getContext(), android.R.layout.simple_list_item_1, twdData.Temporadas);
+
+        setListAdapter(adapter);
+
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
+
+
 
 }
